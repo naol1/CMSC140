a19f789eee20e45c830aff41370c0d66ecedbe87
Naol Tesfaye <81456232+naol1@users.noreply.github.com>
2023-09-13 11:44:10 -0400

    Create Ngobena.cpp

---
+++ Ngobena.cpp
@@ -0,0 +1,28 @@
+/*
+ * Class: CMSC140 CRN 22214
+ * Instructor: Sandro Fouche
+ * Project<>
+ * Description: (Give a brief description for Project)
+ * Due Date:
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any student.
+   Print your Name here: Naol Gobena
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+const string name = "Naol Gobena";
+const string major = "Computer Science";
+
+int main ()
+{
+  string phoneNumber = "243-234-653";
+  string Address = "8830 piney branch rd";
+  cout << name << "\n" << Address << "\n" << phoneNumber << "\n" << major;
+
+  return 0;
+}
